4b7c8f1f3420603ddfbfd1975686674903f88ba9
Ido <Idosabach11@gmail.com>
2023-11-03 02:55:49 +0200

    add js

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
+const myLibrary = []
+
+function Book(name,author,number){
+  this.name = name
+  this.author = author
+  this.number = number
+}
+
+
 const addBook = document.querySelector('.btn-of-add')
 const popup = document.querySelector('.popup')
 const close = document.querySelector('.close')
+const submit = document.querySelector('.submit')
+const grid = document.querySelector('.grid')
+
+const nameInput = document.getElementById('name')
+const authorInput = document.getElementById('author')
+const numberInput = document.getElementById('number')
+
+const readBtnOnFun = document.createElement('button')
+
+readBtnOnFun.addEventListener('click',function(){
+  if(readBtnOnFun.style.backgroundColor='#7ae582'){
+    readBtnOnFun.style.backgroundColor ='#ff8fab'
+    readBtnOnFun.textContent = 'Not read'
+  }
+})
+
+
+
+const removeBtnOnFun = document.createElement('button')
 
 addBook.addEventListener('click',function(){
   popup.style.display = 'flex'
@@ -9,3 +37,38 @@ addBook.addEventListener('click',function(){
 close.addEventListener('click',function(){
   popup.style.display = 'none'
 })
+
+submit.addEventListener('click',addBookToLibrary);
+
+function addBookToLibrary(){
+  //create parent grid
+  const box = document.createElement('div')
+  box.className = 'box'
+  grid.appendChild(box)
+  //create name
+  const name = document.createElement('p')
+  name.className = 'name'
+  name.textContent = nameInput.value
+  box.appendChild(name)
+  //create name
+  const author = document.createElement('p')
+  author.className = 'author'
+  author.textContent = authorInput.value
+  box.appendChild(author)
+  //create name
+  const number = document.createElement('p')
+  number.className = 'number'
+  number.textContent = numberInput.value
+  box.appendChild(number)
+  //create btn read
+  readBtnOnFun.className = 'read-or-not'
+  readBtnOnFun.textContent = 'Read'
+  box.appendChild(readBtnOnFun)
+  //create btn remove
+  removeBtnOnFun.className = 'remove-box'
+  removeBtnOnFun.textContent = 'Remove'
+  box.appendChild(removeBtnOnFun)
+  //close box
+  popup.style.display = 'none'
+}
+
