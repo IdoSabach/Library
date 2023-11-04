472dfeba0c3bc5a867811ea48451176523085cfc
Ido <Idosabach11@gmail.com>
2023-11-04 19:55:14 +0200

    fix main function

--- script.js
+++ script.js
@@ -35,21 +35,25 @@ function addBookToArr(){
   box.appendChild(number)
 
   //create btn read
+  const readBtnOnFun = document.createElement('button')
   readBtnOnFun.className = 'read-or-not'
   readBtnOnFun.textContent = 'Read'
   box.appendChild(readBtnOnFun)
 
   //create btn remove
+  const removeBtnOnFun = document.createElement('button')
   removeBtnOnFun.className = 'remove-box'
   removeBtnOnFun.textContent = 'Remove'
   box.appendChild(removeBtnOnFun)
   //close box
+
+  nameInput.value = ''
+  authorInput.value = ''
+  numberInput.value = ''
+
   popup.style.display = 'none'
+  
 }
-
-
-
-
 
 
 const addBook = document.querySelector('.btn-of-add')
@@ -57,20 +61,10 @@ const popup = document.querySelector('.popup')
 const close = document.querySelector('.close')
 const submit = document.querySelector('.submit')
 const grid = document.querySelector('.grid')
-
+// const form = document.querySelector('.add-book-form').reset()
 const nameInput = document.getElementById('name')
 const authorInput = document.getElementById('author')
 const numberInput = document.getElementById('number')
-
-const readBtnOnFun = document.createElement('button')
-const removeBtnOnFun = document.createElement('button')
-
-readBtnOnFun.addEventListener('click',function(){
-  if(readBtnOnFun.style.backgroundColor='#7ae582'){
-    readBtnOnFun.style.backgroundColor ='#ff8fab'
-    readBtnOnFun.textContent = 'Not read'
-  }
-})
 
 
 addBook.addEventListener('click',function(){
@@ -81,38 +75,12 @@ close.addEventListener('click',function(){
   popup.style.display = 'none'
 })
 
-submit.addEventListener('click',addBookToArr);
+// submit.addEventListener('click',function(event){
+//   event.preventDefault()
+//   addBookToArr();
+// });
+
+submit.addEventListener('click',addBookToArr)
 
 
-// function addBookToLibrary(){
-//   //create parent grid
-//   const box = document.createElement('div')
-//   box.className = 'box'
-//   grid.appendChild(box)
-//   //create name
-//   const name = document.createElement('p')
-//   name.className = 'name'
-//   name.textContent = nameInput.value
-//   box.appendChild(name)
-//   //create name
-//   const author = document.createElement('p')
-//   author.className = 'author'
-//   author.textContent = authorInput.value
-//   box.appendChild(author)
-//   //create name
-//   const number = document.createElement('p')
-//   number.className = 'number'
-//   number.textContent = numberInput.value
-//   box.appendChild(number)
-//   //create btn read
-//   readBtnOnFun.className = 'read-or-not'
-//   readBtnOnFun.textContent = 'Read'
-//   box.appendChild(readBtnOnFun)
-//   //create btn remove
-//   removeBtnOnFun.className = 'remove-box'
-//   removeBtnOnFun.textContent = 'Remove'
-//   box.appendChild(removeBtnOnFun)
-//   //close box
-//   popup.style.display = 'none'
-// }
 
